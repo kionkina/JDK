0dc23af71ca1623d7770db011da35996e19df88f
judyliu2 <jliu15@stuy.edu>
2017-04-02 11:01:47 -0400

    End methods

--- DLLDeque.java
+++ DLLDeque.java
@@ -1,4 +1,4 @@
-public class DLLDeque{
+public class DLLDeque<T>{
 
     private DLLNode<T> _front;
     private DLLNode<T> _end;
@@ -14,13 +14,13 @@ public class DLLDeque{
 //adds element to the front                                                                     
     public void addFront(T x){
 	if(isEmpty()){
-	    _front = new DLLNode<T>(x, null);
+	    _front = new DLLNode<T>(x, null, null);
 	 	}
 	else {
-	    DLLNode bob = new DLLNode<T>(x, null);
-	    bob.setNext(_front);
+	    DLLNode bob = new DLLNode<T>(x, _front ,null);
 	    _front.setPrev(bob);
 	    _front = bob;
+	    _end.setNext(_front);
 	}
 	_size++;
     }
@@ -32,6 +32,8 @@ public class DLLDeque{
 	T ret = _front.getCargo();
 	_front = _front.getNext();
 	_front.setPrev(null);
+	_end.setNext(_front);
+	_size --;
 	return ret;
     }
 
@@ -42,9 +44,29 @@ public class DLLDeque{
 
     //~~~~~~~~~~~~ methods for the End of Deque ~~~~~~~~~~~                                         
 
-    public void addEnd(T x);
-
-    public T removeEnd();
+    public void addEnd(T x){
+	if (isEmpty()){
+	    _end = new DLLNode<T>(x,null, null);
+	}
+	else{
+	    DLLNode<T> bob = new DLLNode<T>(x, _front, _end);
+	    _end.setNext(bob);
+	    _end = bob;
+	}
+	_size ++;
+    }
+    public T removeEnd(){
+	if (isEmpty()){
+	    return null;
+	}
+	else{
+	    T ret = _end.getCargo();
+	    _end = _end.getPrev();
+	    _end.setNext(_front);
+	    _size --;
+	    return ret;
+	}
+    }
 
     public T peekEnd(){
 	return _end.getCargo();
